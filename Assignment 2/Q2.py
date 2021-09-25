00dec7325fac6701bbe0573e4e9dc0043db0cd3f
tukarambhalerao2000 <89186203+tukarambhalerao2000@users.noreply.github.com>
2021-09-25 20:34:08 +0530

    Create Q2.py

---
+++ Assignment 2/Q2.py
@@ -0,0 +1,11 @@
+name=str(input(" Enter your name "))
+mobile=str(input(" Enter your mobile number "))
+emai=str(input(" Enter your email id "))
+address=str(input(" Enter your address "))
+blood=str(input(" Enter your ablood group "))
+
+print("-------------------ID Card----------------------")
+
+print("Name :-  ",name,"\t\tMobile No :- ",mobile)
+print("Email ID :- ",emai,"\t Address :- ",address)
+print("Blood Group :- ",blood)
